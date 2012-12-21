ea44b2c4491544cfbde6c5779b8abedc03fac97f
Squirrelo <ihate@you.com>
2012-12-20 21:34:07 -0600

    updated to 2.2.10

--- md5.sh
+++ md5.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 for f in `find . -type f | grep -v .git | grep -v .DS_ | sed 's/^..//'`
 do
-  echo "`md5 -q $f`|$f"
+  if [ -f /usr/bin/md5sum ]
+  then
+    echo "`md5sum $f | awk '{ print $1 }'`|$f"
+  else  
+    echo "`md5 -q $f`|$f"
+  fi
 done
