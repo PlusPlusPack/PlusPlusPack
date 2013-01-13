c4ebbcd4f54cb61ca9be1430d0706daff08a1e2f
Nathaniel Michael <natecmichael@gmail.com>
2013-01-13 11:04:06 -0800

    Finished merging, fixed md5 spaces problem

--- md5.sh
+++ md5.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
 for f in `find . -type f | grep -v .git | grep -v .DS_ | sed 's/^..//'`
 do
   if [ -f /usr/bin/md5sum ]
   then
     echo "`md5sum $f | awk '{ print $1 }'`|$f"
   else  
-    echo "`md5 -q $f`|$f"
+    echo "`md5 -q \"$f\"`|$f"
   fi
 done
+IFS=$SAVEIFS
